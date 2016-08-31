3daf9f84e4ff9890182daeeb449eae0ac6953717
RakanNimer <rakannimer@gmail.com>
2016-08-31 11:06:49 +0300

    Added Android support. Missing progress tracking.

---
+++ android/src/main/java/com/rnim/rn/audio/AudioPlayerManager.java
@@ -0,0 +1,209 @@
+package com.rnim.rn.audio;
+
+import android.Manifest;
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.io.FileInputStream;
+
+ class AudioPlayerManager extends ReactContextBaseJavaModule {
+
+     private Context context;
+     private MediaPlayer mediaPlayer;
+     private String currentFileName;
+     private int currentPosition;
+     private boolean isPlaying = false;
+     private boolean isPaused = false;
+     private static final String DocumentDirectoryPath = "DocumentDirectoryPath";
+     private static final String PicturesDirectoryPath = "PicturesDirectoryPath";
+     private static final String MainBundlePath = "MainBundlePath";
+     private static final String CachesDirectoryPath = "CachesDirectoryPath";
+     private static final String LibraryDirectoryPath = "LibraryDirectoryPath";
+     private static final String MusicDirectoryPath = "MusicDirectoryPath";
+     private static final String DownloadsDirectoryPath = "DownloadsDirectoryPath";
+
+
+     public AudioPlayerManager(ReactApplicationContext reactContext) {
+         super(reactContext);
+         this.context = reactContext;
+     }
+
+
+     @Override
+     public String getName() {
+         return "AudioPlayerManager";
+     }
+
+
+     @Override
+     public Map<String, Object> getConstants() {
+       Map<String, Object> constants = new HashMap<>();
+
+       constants.put(DocumentDirectoryPath, this.getReactApplicationContext().getFilesDir().getAbsolutePath());
+       constants.put(PicturesDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
+       constants.put(MainBundlePath, "");
+       constants.put(CachesDirectoryPath, this.getReactApplicationContext().getCacheDir().getAbsolutePath());
+       constants.put(LibraryDirectoryPath, "");
+       constants.put(MusicDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath());
+       constants.put(DownloadsDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath());
+       return constants;
+     }
+
+
+     @ReactMethod
+     public void getDuration(Promise promise){
+       if (mediaPlayer == null){
+           Log.e("PLAYER_NOT_PREPARED", "Please call startPlaying before stopping playback");
+           promise.reject("PLAYER_NOT_PREPARED", "Please call startPlaying before stopping playback");
+           return;
+       }
+       promise.resolve(mediaPlayer.getDuration());
+
+     }
+
+     @ReactMethod
+     public void stopPlaying(Promise promise){
+         if (!isPlaying){
+             Log.e("INVALID_STATE", "Please call play or playWithURL before stopping playback");
+             promise.reject("INVALID_STATE", "Please call play or playWithURL before stopping playback");
+             return;
+         }
+       mediaPlayer.stop();
+       mediaPlayer.release();
+       promise.resolve(currentFileName);
+     }
+
+     @ReactMethod
+     public void pause(Promise promise){
+         if (!isPlaying){
+             Log.e("INVALID_STATE", "Please call play or playWithURL before pausing playback");
+             promise.reject("INVALID_STATE", "Please call play or playWithURL before pausing playback");
+             return;
+         }
+
+       mediaPlayer.pause();
+       currentPosition = mediaPlayer.getCurrentPosition();
+       isPaused = true;
+       isPlaying = false;
+       promise.resolve(currentFileName);
+     }
+
+     @ReactMethod
+     public void unpause(Promise promise){
+         if (!isPaused){
+             Log.e("INVALID_STATE", "Please call pause before unpausing playback");
+             promise.reject("INVALID_STATE", "Please call pause before unpausing playback");
+             return;
+         }
+         mediaPlayer.seekTo(currentPosition);
+         mediaPlayer.start();
+         promise.resolve(currentFileName);
+     }
+
+     @ReactMethod
+     public void playFromPath(String path, final Promise promise) {
+        if (isPlaying){
+            Log.e("INVALID_STATE", "Please wait for previous playback to finish.");
+            promise.reject("INVALID_STATE", "Please set valid path");
+            return;
+        }
+        if (path == null){
+          Log.e("INVALID_STATE", "Please set valid path");
+          promise.reject("INVALID_STATE", "Please set valid path");
+          return;
+        }
+
+         mediaPlayer = new MediaPlayer();
+         playMedia("local", path, promise);
+         return;
+     }
+
+     @ReactMethod
+     public void playFromURL(String url, final Promise promise) {
+         playMedia("remote", url, promise);
+         return;
+     }
+
+    private void playMedia(String type, final String path, final Promise promise){
+       boolean playbackReady = preparePlaybackAtPath(type, path, promise);
+       if (!playbackReady){
+           return;
+       }
+
+       mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+           public void onCompletion(MediaPlayer mp) {
+               promise.resolve(path);
+               mediaPlayer.stop();
+               mediaPlayer.release();
+               mediaPlayer = null;
+           }
+       });
+
+       mediaPlayer.start();
+   }
+
+   private boolean preparePlaybackAtPath(String pathType, String path, Promise promise) {
+     if (mediaPlayer == null) {
+       mediaPlayer = new MediaPlayer();
+     }
+     else {
+       mediaPlayer.reset();
+     }
+
+     mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+     try {
+       if (pathType == "local"){
+         FileInputStream fis = new FileInputStream(new File(path));
+         mediaPlayer.setDataSource(fis.getFD());
+       }
+       else if (pathType == "remote"){
+         mediaPlayer.setDataSource(path);
+       }
+
+     } catch (IllegalArgumentException e) {
+         promise.reject("COULDNT_PREPARE_MEDIAPLAYER", e.getMessage());
+         e.printStackTrace();
+    } catch (SecurityException e) {
+         promise.reject("COULDNT_PREPARE_MEDIAPLAYER", e.getMessage());
+         e.printStackTrace();
+    } catch (IllegalStateException e) {
+         promise.reject("COULDNT_PREPARE_MEDIAPLAYER", e.getMessage());
+    } catch (IOException e) {
+         promise.reject("COULDNT_PREPARE_MEDIAPLAYER", e.getMessage());
+         e.printStackTrace();
+    }
+    try {
+      mediaPlayer.prepare();
+      return true;
+
+    } catch (IllegalStateException e) {
+      promise.reject("COULDNT_PREPARE_MEDIAPLAYER",e.getMessage());
+      e.printStackTrace();
+    } catch (IOException e) {
+      promise.reject("COULDNT_PREPARE_MEDIAPLAYER",e.getMessage());
+      e.printStackTrace();
+    }
+    return false;
+   }
+
+ }
---
+++ android/src/main/java/com/rnim/rn/audio/AudioRecorderManager.java
@@ -0,0 +1,170 @@
+package com.rnim.rn.audio;
+
+import android.Manifest;
+import android.content.Context;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import java.io.FileInputStream;
+
+class AudioRecorderManager extends ReactContextBaseJavaModule {
+  private Context context;
+  private MediaRecorder recorder;
+
+  private String currentOutputFile;
+  private boolean isRecording = false;
+  private AudioPlayerManager audioPlayerManager;
+  private static final String DocumentDirectoryPath = "DocumentDirectoryPath";
+  private static final String PicturesDirectoryPath = "PicturesDirectoryPath";
+  private static final String MainBundlePath = "MainBundlePath";
+  private static final String CachesDirectoryPath = "CachesDirectoryPath";
+  private static final String LibraryDirectoryPath = "LibraryDirectoryPath";
+  private static final String MusicDirectoryPath = "MusicDirectoryPath";
+  private static final String DownloadsDirectoryPath = "DownloadsDirectoryPath";
+
+
+  public AudioRecorderManager(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.context = reactContext;
+    this.audioPlayerManager = new AudioPlayerManager(reactContext);
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    Map<String, Object> constants = new HashMap<>();
+
+    constants.put(DocumentDirectoryPath, this.getReactApplicationContext().getFilesDir().getAbsolutePath());
+    constants.put(PicturesDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath());
+    constants.put(MainBundlePath, "");
+    constants.put(CachesDirectoryPath, this.getReactApplicationContext().getCacheDir().getAbsolutePath());
+    constants.put(LibraryDirectoryPath, "");
+    constants.put(MusicDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath());
+    constants.put(DownloadsDirectoryPath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath());
+    return constants;
+  }
+
+  @Override
+  public String getName() {
+    return "AudioRecorderManager";
+  }
+
+  @ReactMethod
+  public void checkAuthorizationStatus(Promise promise) {
+    int permissionCheck = ContextCompat.checkSelfPermission(getCurrentActivity(),
+            Manifest.permission.RECORD_AUDIO);
+    boolean permissionGranted = permissionCheck == PackageManager.PERMISSION_GRANTED;
+    promise.resolve(permissionGranted);
+  }
+
+  @ReactMethod
+  public void prepareRecordingAtPath(String recordingPath, ReadableMap recordingSettings, Promise promise) {
+    if (isRecording){
+      Log.e("INVALID_STATE", "Please call stopRecording before starting recording");
+      promise.reject("INVALID_STATE", "Please call stopRecording before starting recording");
+    }
+
+    recorder = new MediaRecorder();
+    try {
+      recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+      recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+      recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+      recorder.setAudioSamplingRate(recordingSettings.getInt("SampleRate"));
+      recorder.setAudioChannels(recordingSettings.getInt("Channels"));
+      recorder.setAudioEncodingBitRate(32000); //recordingSettings.getInt("EncodingBitRate"));
+      recorder.setOutputFile(recordingPath);
+    }
+    catch(final Exception e) {
+      promise.reject("COULDNT_CONFIGURE_AUDIO_SOURCE" , "Make sure you've added RECORD_AUDIO permission to your AndroidManifest.xml file "+e.getMessage());
+      return;
+    }
+
+    currentOutputFile = recordingPath;
+    try {
+      recorder.prepare();
+      promise.resolve(currentOutputFile);
+    } catch (final Exception e) {
+      promise.reject("COULDNT_PREPARE_RECORDING_AT_PATH "+recordingPath, e.getMessage());
+    }
+
+  }
+
+  @ReactMethod
+  public void startRecording(Promise promise){
+    if (recorder == null){
+      Log.e("RECORDING_NOT_PREPARED", "Please call prepareRecordingAtPath before starting recording");
+      promise.reject("RECORDING_NOT_PREPARED", "Please call prepareRecordingAtPath before starting recording");
+      return;
+    }
+    if (isRecording){
+      Log.e("INVALID_STATE", "Please call stopRecording before starting recording");
+      promise.reject("INVALID_STATE", "Please call stopRecording before starting recording");
+      return;
+    }
+    recorder.start();
+    isRecording = true;
+    promise.resolve(currentOutputFile);
+  }
+
+  @ReactMethod
+  public void stopRecording(Promise promise){
+    if (!isRecording){
+      Log.e("INVALID_STATE", "Please call startRecording before stopping recording");
+      promise.reject("INVALID_STATE", "Please call startRecording before stopping recording");
+      return;
+    }
+    recorder.stop();
+    isRecording = false;
+    recorder.release();
+    promise.resolve(currentOutputFile);
+  }
+
+  @ReactMethod
+  public void pauseRecording(Promise promise){
+    // Added this function to have the same api for android and iOS, stops recording now
+    stopRecording(promise);
+  }
+
+  @ReactMethod
+  public void stopPlaying(Promise promise){
+    audioPlayerManager.stopPlaying(promise);
+  }
+
+  @ReactMethod
+  public void pausePlaying(Promise promise){
+    audioPlayerManager.pause(promise);
+  }
+
+  @ReactMethod
+  public void unpausePlaying(Promise promise){
+    audioPlayerManager.unpause(promise);
+  }
+
+  @ReactMethod
+  public void playRecording(final Promise promise) {
+    audioPlayerManager.playFromPath(currentOutputFile, promise);
+  }
+
+
+
+
+
+}
