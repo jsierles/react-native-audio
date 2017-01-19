394df41628dfffbdc7625360cb06e308a22123d7
Grace Han <hanpanpan200@gmail.com>
2017-01-19 10:43:51 +0800

    feat: init example code

---
+++ Example/index.ios.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native'
+import Example from './src/Example'
+
+AppRegistry.registerComponent('Example', () => Example)
---
+++ Example/src/Example.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {
+  View
+} from 'react-native'
+
+export default class Example extends Component {
+  render() {
+    return (
+      <View></View>
+    )
+  }
+}
