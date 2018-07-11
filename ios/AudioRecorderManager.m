0e314be69a2777ef4a29e0d201a435437efa8696
Steven Masini <steven.masini@icloud.com>
2018-07-11 14:38:46 +0800

    feat: resume the music/audio player once recorder has stopped recording

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -91,6 +91,17 @@ RCT_EXPORT_MODULE();
       @"status": flag ? @"OK" : @"ERROR",
       @"audioFileURL": [_audioFileURL absoluteString]
     }];
+    
+    // This will resume the music/audio file that was playing before the recording started
+    // Without this piece of code, the music/audio will just be stopped
+    NSError *error;
+    [[AVAudioSession sharedInstance] setActive:NO
+                                   withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation
+                                         error:&error];
+    if (error) {
+        // TODO: dispatch error over the bridge
+        NSLog(@"error: %@", [error localizedDescription]);
+    }
 }
 
 - (NSString *) applicationDocumentsDirectory
