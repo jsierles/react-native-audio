a15130aa8a4e76ce7ecf27b3a2f13af6294ef9e9
夏培旭 <18530815565@163.com>
2019-05-14 14:17:12 +0800

    update TypeScript type definition

--- index.d.ts
+++ index.d.ts
@@ -1,47 +1,46 @@
 export interface IRecordingOptions {
-	SampleRate?: number;
-	Channels?: number;
-	AudioQuality?: string;
-	AudioEncoding?: string;
-	OutputFormat?: string;
-	MeteringEnabled?: boolean;
-	MeasurementMode?: boolean;
-	AudioEncodingBitRate?: number;
-	IncludeBase64?: boolean;
-	AudioSource?: number;
-  }
-  
-  export var AudioRecorder: {
-	requestAuthorization(): Promise<boolean>;
-	prepareRecordingAtPath(path: string, options: IRecordingOptions): Promise<string>;
-	startRecording(): Promise<string>;
-	stopRecording(): Promise<string>;
-	resumeRecording(): Promise<string>;
-	pauseRecording(): Promise<string>;
-	checkAuthorizationStatus(): Promise<boolean>;
-	onProgress(res: { currentTime: number; }): void;
-	onFinished(res: { audioFileURL: string; base64: string; status: string; }): void;
-  }
-  
-  export var AudioUtils: {
-	CachesDirectoryPath: string;
-	DocumentDirectoryPath: string;
-	DownloadsDirectoryPath: string;
-	LibraryDirectoryPath: string;
-	MainBundlePath: string;
-	MusicDirectoryPath: string;
-	PicturesDirectoryPath: string;
-  };
-  export var AudioSource: {
-	CAMCORDER: number;
-	DEFAULT: number;
-	MIC: number;
-	REMOTE_SUBMIX: number;
-	UNPROCESSED: number;
-	VOICE_CALL: number;
-	VOICE_COMMUNICATION: number;
-	VOICE_DOWNLINK: number;
-	VOICE_RECOGNITION: number;
-	VOICE_UPLINK: number;
-  };
-  
+  SampleRate?: number;
+  Channels?: number;
+  AudioQuality?: 'Low' | 'Medium' | 'High';
+  AudioEncoding?: 'lpcm' | 'ima4' | 'aac' | 'MAC3' | 'MAC6' | 'ulaw' | 'alaw' | 'mp1' | 'mp2' | 'alac' | 'amr' | 'aac' | 'aac_eld' | 'amr_nb' | 'amr_wb' | 'he_aac' | 'vorbis';
+  OutputFormat?: string;
+  MeteringEnabled?: boolean;
+  MeasurementMode?: boolean;
+  AudioEncodingBitRate?: number;
+  IncludeBase64?: boolean;
+  AudioSource?: number;
+}
+
+export var AudioRecorder: {
+  requestAuthorization(): Promise<boolean>;
+  prepareRecordingAtPath(path: string, options: IRecordingOptions): Promise<string>;
+  startRecording(): Promise<string>;
+  stopRecording(): Promise<string>;
+  resumeRecording(): Promise<string>;
+  pauseRecording(): Promise<string>;
+  checkAuthorizationStatus(): Promise<boolean>;
+  onProgress(res: { currentTime: number; }): void;
+  onFinished(res: { audioFileURL: string; base64: string; status: string; }): void;
+}
+
+export var AudioUtils: {
+  CachesDirectoryPath: string;
+  DocumentDirectoryPath: string;
+  DownloadsDirectoryPath: string;
+  LibraryDirectoryPath: string;
+  MainBundlePath: string;
+  MusicDirectoryPath: string;
+  PicturesDirectoryPath: string;
+};
+export var AudioSource: {
+  CAMCORDER: number;
+  DEFAULT: number;
+  MIC: number;
+  REMOTE_SUBMIX: number;
+  UNPROCESSED: number;
+  VOICE_CALL: number;
+  VOICE_COMMUNICATION: number;
+  VOICE_DOWNLINK: number;
+  VOICE_RECOGNITION: number;
+  VOICE_UPLINK: number;
+};
